899d559329a3683e5bfb5fb3be1bdda4ebe91cda
Samuel Sobreira <samuel.sobreira@camara.leg.br>
2025-04-30 17:49:14 -0300

    merge

---
+++ merge_sort.py
@@ -0,0 +1,11 @@
+#Divida o array ao meio recursivamente e depois intercala os subarrays ordenados.
+import random
+
+qtn = 700
+lista = []
+for i in range(qtn):
+    lista.append(random.randint(1,700))
+
+
+    
+print(lista)
